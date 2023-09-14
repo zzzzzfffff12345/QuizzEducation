9b4bd3e3645fb8c76424cb0114cc570b773ace7b
Thien-CG <lethien3072003@gmail.com>
2023-09-14 09:54:42 +0700

    first commit

---
+++ quizz_education_be/src/main/java/com/vnpt/quizz_education_be/QuizzEducationBeApplication.java
@@ -0,0 +1,13 @@
+package com.vnpt.quizz_education_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzEducationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzEducationBeApplication.class, args);
+	}
+
+}
---
+++ quizz_education_be/src/test/java/com/vnpt/quizz_education_be/QuizzEducationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnpt.quizz_education_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzEducationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
