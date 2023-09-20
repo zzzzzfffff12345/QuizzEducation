fb884ef819c04a7097ce0466a8cd43aa63eac3c6
2410thao <88305027+2410thao@users.noreply.github.com>
2023-09-20 16:44:15 +0700

    Update Final API

--- quizz_education_be/src/main/java/com/vnpt/quizz_education_be/RestController/ChiTietKyThiRestController.java
+++ quizz_education_be/src/main/java/com/vnpt/quizz_education_be/RestController/ChiTietKyThiRestController.java
@@ -1,11 +1,17 @@
 package com.vnpt.quizz_education_be.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +29,44 @@ public class ChiTietKyThiRestController {
     public ResponseEntity<List<ChiTietKyThi>> findAll() {
         return ResponseEntity.ok(chiTietKyThiDAO.findAll());
     }
+    // Get 1 đối tượng thông qua id
+    @GetMapping("chitietkythi/{id}")
+    public ResponseEntity<ChiTietKyThi> findById(@PathVariable("id") Integer maChiTietKyThi) {
+        Optional<ChiTietKyThi> optional = chiTietKyThiDAO.findById(maChiTietKyThi);
+        if (!optional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optional.get());
+    }
+
+    @PostMapping("chitietkythi")
+    public ResponseEntity<ChiTietKyThi> post(@RequestBody ChiTietKyThi chitietkythi) {
+        if (chiTietKyThiDAO.existsById(chitietkythi.getMaChiTietKyThi())) {
+            return ResponseEntity.badRequest().build();
+            // 400 Bad Request: Địa chỉ tồi
+        }
+        ChiTietKyThi chitietkythi2 = chiTietKyThiDAO.save(chitietkythi);
+        return ResponseEntity.ok(chitietkythi2);
+    }
+
+    @PutMapping("chitietkythi/{id}")
+    public ResponseEntity<ChiTietKyThi> put(@PathVariable("id") Integer maChiTietKyThi, @RequestBody ChiTietKyThi chitietkythi) {
+        if (!chiTietKyThiDAO.existsById(maChiTietKyThi)) {
+            return ResponseEntity.notFound().build();
+        }
+        chiTietKyThiDAO.save(chitietkythi);
+        return ResponseEntity.ok(chitietkythi);
+    }
+
+    @DeleteMapping("chitietkythi/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer maChiTietKyThi) {
+        if (!chiTietKyThiDAO.existsById(maChiTietKyThi)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        chiTietKyThiDAO.deleteById(maChiTietKyThi);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
--- quizz_education_be/src/main/java/com/vnpt/quizz_education_be/RestController/KyThiRestController.java
+++ quizz_education_be/src/main/java/com/vnpt/quizz_education_be/RestController/KyThiRestController.java
@@ -1,11 +1,17 @@
 package com.vnpt.quizz_education_be.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +31,43 @@ public class KyThiRestController {
         return ResponseEntity.ok(kyThiDAO.findAll());
     }
 
+    // Get 1 đối tượng thông qua id
+    @GetMapping("kythi/{id}")
+    public ResponseEntity<KyThi> findById(@PathVariable("id") Integer maKyThi) {
+        Optional<KyThi> optional = kyThiDAO.findById(maKyThi);
+        if (!optional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(optional.get());
+    }
+
+    @PostMapping("kythi")
+    public ResponseEntity<KyThi> post(@RequestBody KyThi kythi) {
+        if (kyThiDAO.existsById(kythi.getMaKyThi())) {
+            return ResponseEntity.badRequest().build();
+            // 400 Bad Request: Địa chỉ tồi
+        }
+        KyThi kythi2 = kyThiDAO.save(kythi);
+        return ResponseEntity.ok(kythi2);
+    }
+
+    @PutMapping("kythi/{id}")
+    public ResponseEntity<KyThi> put(@PathVariable("id") Integer maKyThi, @RequestBody KyThi kythi) {
+        if (!kyThiDAO.existsById(maKyThi)) {
+            return ResponseEntity.notFound().build();
+        }
+        kyThiDAO.save(kythi);
+        return ResponseEntity.ok(kythi);
+    }
+
+    @DeleteMapping("kythi/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer maKyThi) {
+        if (!kyThiDAO.existsById(maKyThi)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        kyThiDAO.deleteById(maKyThi);
+
+        return ResponseEntity.ok().build();
+    }
 }
